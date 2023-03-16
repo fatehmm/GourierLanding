32f8240228f57540888221c0f5b53919444c7bab
Fateh Mammadli <fatehshm@code.edu.az>
2023-03-16 16:34:48 +0400

    qweqwe

---
+++ main.js
@@ -0,0 +1,20 @@
+//MOBILE BUTTON CLOSE OPEN 
+const menuCloseButton = document.getElementById('mobile-close-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const openMenu = document.getElementById('open-menu-button');
+menuCloseButton.addEventListener('click', function () {
+    mobileMenu.classList.remove('opacity-100', 'scale-100');
+    mobileMenu.classList.add('opacity-0', 'scale-95');
+    setTimeout(() => {
+        mobileMenu.style.display = 'none'
+    }, 300);
+    
+})
+openMenu.addEventListener('click', function (){
+    
+    mobileMenu.style.display = 'block';
+    setTimeout(() => {
+        mobileMenu.classList.remove('opacity-0', 'scale-95');
+    mobileMenu.classList.add('opacity-100', 'scale-100');
+    }, 10);
+})
